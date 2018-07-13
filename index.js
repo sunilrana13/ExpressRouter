b3b1d3341963617c458eb148fc515eb70fcc5466
Sunil Rana <muskaan.rana@gmail.com>
2018-07-13 18:47:12 +0530

    express router commit2

--- index.js
+++ index.js
@@ -2,26 +2,23 @@ const hostname = "nodeapp.com";
 const port = 3000;
 const http = require("http");
 const express = require("express");
+
+
 const dishrouter = require("./routes/dishrouter");
 const courserouter = require("./routes/courserouter");
 const app = express();
 
-app.use(express.static(__dirname + "/public"));
-
-//app.all("/dishes", (req, res, next) => {
-//    res.statusCode = 200;
-//    res.setHeader("content-type", "text/html");
-//    next();
-//})
-//app.get("/dishes", (req, res, next) => {
-//    res.end("get request received and responded!!");
-//});
-//app.post("/dishes", (req, res, next) => {
-//    res.end("post request received and responded!!");
-//});
 
 app.use("/dishes", dishrouter);
 app.use("/course", courserouter);
+app.use(express.static(__dirname + "/public"));
+
+app.use((req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader("content-type", "text/html");
+    res.end("<H1> This is an Express Router Demo App!!</H1>");
+})
+
 
 const server = http.createServer(app);
 
--- routes/dishrouter.js
+++ routes/dishrouter.js
@@ -1,5 +1,9 @@
 const express = require("express");
+const bodyparser = require("body-parser");
+
 const router = express.Router();
+router.use(bodyparser.json());
+
 router.route('/').
 all((req, res, next) => {
     res.statusCode = 200;
@@ -7,10 +11,17 @@ all((req, res, next) => {
     next();
 }).
 get((req, res, next) => {
-    res.end("get request received and responded!!");
+    res.end("get request received and responded -- all dishes!!");
 }).
 post((req, res, next) => {
-    res.end("post request received and responded!!");
+    res.end("post request received and responded -- dish: "+ req.body.name+"description: "+ req.body.description);
+}).
+    put((req, res, next) => {
+        res.stat = 403;
+        res.end("put request received and responded -- Put operation not supported!!");
+}).
+delete((req, res, next) => {
+    res.end("delete request received and responded -- -- All dishes deleted!!");
 });
 
 router.route('/:dishid').
@@ -23,6 +34,13 @@ router.route('/:dishid').
         res.end("get request received and responded for dishid= " + req.params.dishid);
     }).
     post((req, res, next) => {
-        res.end("post request received and respondedfor dishid= " + req.params.dishid);
+        res.stat = 403;
+        res.end("post request received and responded -- Post operation not supported!!");
+    }).
+    put((req, res, next) => {
+        res.end("put request received and responded -- dishid= " + req.params.dishid + " dish: " + req.body.name + " description: " + req.body.description);
+    }).
+    delete((req, res, next) => {
+        res.end("delete request received and responded for dishid= " + req.params.dishid);
     });
 module.exports = router;
